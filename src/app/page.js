a45121b65199f5f6b247f368dbcf13b52acbc578
jimbobding <jimmygrahamdavid@gmail.com>
2024-02-22 15:11:20 +0000

    added loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,13 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page1`,
+    }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
-
   const data = await res.json();
   if (!res.ok) {
-    throw new Error("Unable to fetch data");
+    throw new Error("Failed to fetch data");
   }
-
   const results = data.results;
 
   return (
